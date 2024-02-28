0579c08bb9a9a43d58ee87aaf9a24a9d16baefe0
Biruk Bogale <bbuurraa10@gmail.com>
2024-02-28 15:38:42 +0300

    moving to server

--- routes/createTableRoute.js
+++
@@ -1,9 +0,0 @@
-// create tables
-const express = require("express");
-const tableRouter = express.Router();
-
-const { createTable } = require("../controller/createTable");
-
-tableRouter.get("/create", createTable);
-
-module.exports = tableRouter;
